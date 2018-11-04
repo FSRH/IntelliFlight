a0697fbdb235da84cea1adcc198046ec6032946e
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2018-11-04 19:26:38 +0100

    fix uclog include

--- cmake/uclog.cmake
+++ cmake/uclog.cmake
@@ -4,9 +4,9 @@ if (UCLOG_DRIVER_DIR STREQUAL "UCLOG_DRIVER_DIR-NOTFOUND")
     message(FATAL_ERROR "Could not locate lib/uclog directory")
 endif ()
 
-include_directories(${UCLOG_DRIVER_DIR})
+include_directories(${UCLOG_DRIVER_DIR}/include/)
 
-file(GLOB_RECURSE UCLOG_DRIVER_FILES ${UCLOG_DRIVER_DIR}/*.hpp ${UCLOG_DRIVER_DIR}/*.cpp ${UCLOG_DRIVER_DIR}/*.h ${UCLOG_DRIVER_DIR}/*.c)
+file(GLOB_RECURSE UCLOG_DRIVER_FILES "${UCLOG_DRIVER_DIR}/include/*.hpp" "${UCLOG_DRIVER_DIR}/src/*.cpp" "${UCLOG_DRIVER_DIR}/include/*.h" "${UCLOG_DRIVER_DIR}/src/*.c")
 
 add_library(UCLOG STATIC ${UCLOG_DRIVER_FILES})
 
